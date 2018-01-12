a472480ed85efb2fe42fffdfeab316d73dc501af
liubole <liubole@corp-ci.com>
2018-01-12 17:59:43 +0800

    bugfix

--- src/Common/Util.php
+++ src/Common/Util.php
@@ -81,12 +81,18 @@ class Util
         return empty($diff);
     }
 
-    private static function random($even_len)
+    private static function random($length)
     {
-        $data = openssl_random_pseudo_bytes((int)$even_len / 2);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-        return vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4));
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($length / 2));
+            return substr(bin2hex($bytes), 0, $length);
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
+            return substr(bin2hex($bytes), 0, $length);
+        } else {
+            $pool = 'abcdefghijklmnopqrstuvwxyz0123456789';
+            return substr(str_shuffle(str_repeat($pool, ceil($length / strlen($pool)))), 0, $length);
+        }
     }
 
     public static function writeHexLong()
